0b3cffa69529c8b76fa690bc6c24fd59c89abc3b
Ahmed Helali <ahmed@ahmedhelali.xyz>
2026-02-25 21:17:06 +0100

    Update gröbnerbasiswithlex.py
    Added Question 3 from sheet 13

--- compalg/gröbnerbasiswithlex.py
+++ compalg/gröbnerbasiswithlex.py
@@ -7,3 +7,21 @@ I = R.ideal([x^2 + y + z - 1, x + y^2 + z - 1, x + y + z^2 - 1])
 # Compute the Gröbner basis (will use lex ordering)
 G = I.groebner_basis()
 print(G)
+
+# # Sheet 13 Q3
+# # Define the polynomial ring with lex ordering
+# R.<X3, X2, X1> = PolynomialRing(QQ, order='lex')
+
+# # Define the three polynomials
+# f1 = 2*X1 + X2 - 2*X2^2 - X3^2
+# f2 = X1 - X2^2 + X3^2
+# f3 = X1*X2 - X3^2
+
+# # Create the ideal
+# I = R.ideal([f1, f2, f3])
+
+# # Compute the Gröbner basis
+# G = I.groebner_basis()
+
+# print(G)
+
